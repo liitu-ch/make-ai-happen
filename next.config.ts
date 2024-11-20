092a1faa36d5639981d2292da942549b745345d9
sansan88 <sandro.scalco@liitu.ch>
2024-11-20 16:32:52 +0100

    feat

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {};
 const nextConfig = {
+  output: 'standalone',  // Wichtig für Vercel
   reactStrictMode: true,
+  trailingSlash: false, // Keine überflüssigen Slashes
 };
 export default nextConfig;
